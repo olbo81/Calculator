f939c12b301ce3abc113b4af8d61fbe035f38bc9
olbo81 <olbo81@mail.ru>
2021-05-28 12:28:40 +0500

    Initial commit

---
+++ src/ArithmeticOperations.java
@@ -0,0 +1,16 @@
+public enum ArithmeticOperations {
+    DIVISION("/"),
+    MULTIPLICATION("*"),
+    ADDITION("+"),
+    SUBTRACTION("-");
+
+    private final String value;
+
+    ArithmeticOperations(String value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return this.value;
+    }
+}
